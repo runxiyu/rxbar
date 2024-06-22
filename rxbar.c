2cd0a9635fa8341e29231b35f6372c7b63d25cb7
Runxi Yu <me@runxiyu.org>
2024-06-22 08:00:00 +0800

    Aligned usleep by Test_User

--- rxbar.c
+++ rxbar.c
@@ -8,6 +8,7 @@
 #include <time.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdint.h>
 
 #define LEN 10
 
@@ -16,11 +17,15 @@ char buf[LEN];
 struct tm td;
 int fd;
 int i;
+time_t t1;
+time_t t2;
 
 int main()
 {
 	fd = open("/sys/class/power_supply/macsmc-battery/capacity", O_RDONLY);
 	for (;;) {
+		t = time(NULL);
+		td = *localtime(&t);
 		pread(fd, buf, LEN, 0);
 		for (i = 0; i < LEN; ++i) {
 			if (buf[i] == '\n') {
@@ -28,13 +33,16 @@ int main()
 				break;
 			}
 		}
-		t = time(NULL);
-		td = *localtime(&t);
 		dprintf(STDOUT_FILENO, "%s%% %d-%02d-%02d %02d:%02d:%02d\n",
 			buf,
 			td.tm_year + 1900,
 			td.tm_mon + 1,
 			td.tm_mday, td.tm_hour, td.tm_min, td.tm_sec);
-		sleep(1);
+		t = (t + 1) * 1000000;
+		for (t1 = time(NULL) * 1000000; t1 < t;
+		     t1 = time(NULL) * 1000000) {
+			t2 = t - t1;
+			usleep(t2);
+		}
 	}
 }
