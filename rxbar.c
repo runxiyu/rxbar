cb4c4b9000c7b37c17fda74e7dba5db05b5beb9a
Runxi Yu <me@runxiyu.org>
2024-06-25 08:00:00 +0800

    Reformat the long line

--- rxbar.c
+++ rxbar.c
@@ -29,7 +29,16 @@ int main()
 		len = pread(fdc, buf + 1, MAXLEN, 0);
 		pread(fds, buf, 1, 0);
 		buf[len] = '\0';
-		dprintf(STDOUT_FILENO, "%s %d-%02d-%02d %02d:%02d:%02d\n", buf, td.tm_year + 1900, td.tm_mon + 1, td.tm_mday, td.tm_hour, td.tm_min, td.tm_sec);
+		dprintf(STDOUT_FILENO,
+			"%s %d-%02d-%02d %02d:%02d:%02d\n",
+			buf,
+			td.tm_year + 1900,
+			td.tm_mon + 1,
+			td.tm_mday,
+			td.tm_hour,
+			td.tm_min,
+			td.tm_sec
+		);
 		t = (t + 1) * 1000000;
 		for (t1 = time(NULL) * 1000000; t1 < t; t1 = time(NULL) * 1000000)
 			usleep(t - t1);
